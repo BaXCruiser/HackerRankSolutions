73d3ea1b901eb9d42e8b8ff450cf1a6387c4fde6
Deepak Kumar <45505692+BaXCruiser@users.noreply.github.com>
2019-04-06 23:03:15 +0530

    Create AVeryBigSum.py

---
+++ Algorithms/Warmup/AVeryBigSum.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  6 19:03:13 2019
+
+@author: BaX Cruiser
+"""
+
+n=int(input())
+s=input().split(" ")
+Sum=0
+for i in s:
+    Sum+=int(i)
+print(Sum)
