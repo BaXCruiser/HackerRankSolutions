d46b39fc0ca7adfa5435420331116e68c65241c4
Deepak Kumar <45505692+BaXCruiser@users.noreply.github.com>
2020-09-16 13:15:49 +0530

    Update JumpingOnTheClouds.py

--- Algorithms/Implimentation/JumpingOnTheClouds.py
+++ Algorithms/Implimentation/JumpingOnTheClouds.py
@@ -7,11 +7,12 @@ Created on Sat Apr  6 15:12:09 2019
 
 n=int(input())
 arr=input()
-jump=1
-i=0
+jump=0
+i=1
 while(i<n) :
-    jump+=1
-    if(arr[i]!='0' or (arr[i]=='0' and arr[i-2]=='0' and arr[i-1]=='0')):
-        jump-=1
+    if arr[i]==0:
+        jump+=1
+        if(i<len(arr)-2 and arr[i+2]=='0' and arr[i+1]=='0')):
+        i+=1
     i+=1
 print(jump)
